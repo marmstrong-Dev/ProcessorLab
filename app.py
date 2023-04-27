f23f04df2bbfcdb777711dab13c6149e6d7516de
Michael Armstrong <armstrongm270x@icloud.com>
2023-04-26 20:49:24 -0600

    Set Up Dependencies for Flask and MongoDB

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+load_dotenv()
+
+
+@app.route("/api/status")
+def status():
+    return jsonify({"status": "online", "version": 0.1})
+
 
 if __name__ == '__main__':
-    print("API Started")
+    app.run(debug=True, port=os.getenv("PORT"))
